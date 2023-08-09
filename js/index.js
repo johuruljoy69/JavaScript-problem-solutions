30dc9f3dfc4a65fb55dd6d438947dad131c2fec1
johirjoy <joy.sd69@gmail.com>
2023-08-10 01:41:19 +0600

    sucess and waring dialog box

--- js/index.js
+++ js/index.js
@@ -1,9 +1,41 @@
-document.getElementById("open-popup-btn").addEventListener("click", function(){
-    document.getElementsByClassName("dialog-popup")
-    [0].classList.add("active");
+const dialogButtons = document.querySelectorAll('[data-target]');
+const dialogs = document.querySelectorAll('.dialog');
+
+dialogButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const targetDialogId = button.getAttribute('data-target');
+        const targetDialog = document.getElementById(targetDialogId);
+
+        if (targetDialog) {
+            dialogs.forEach(dialog => {
+                dialog.style.display = 'none';
+            });
+
+            targetDialog.style.display = 'block';
+        }
+    });
 });
 
-document.getElementById("dismiss-popup-btn").addEventListener("click", function(){
-    document.getElementsByClassName("dialog-popup")
-    [0].classList.remove("active");
+dialogs.forEach(dialog => {
+    const confirmButton = dialog.querySelector('.confirmBtn');
+    const cancelButton = dialog.querySelector('.cancelBtn');
+    const deleteButton = dialog.querySelector('.deleteBtn');
+
+    if (confirmButton) {
+        confirmButton.addEventListener('click', () => {
+            // Add your confirm logic here
+            dialog.style.display = 'none';
+        });
+    }
+
+    if (cancelButton) {
+        cancelButton.addEventListener('click', () => {
+            dialog.style.display = 'none';
+        });
+    }
+    if (deleteButton) {
+        deleteButton.addEventListener('click', () => {
+            dialog.style.display = 'none';
+        });
+    }
 });
